55420bfd231cdcce5953e69d0d870e3f168406f4
Ingy Badawi <ngbadawi@yahoo.com>
2021-12-30 00:18:25 +0200

    Time: 1068 ms (6.29%), Space: 16.9 MB (36.85%) - LeetHub

--- jump-game-ii/jump-game-ii.cpp
+++ jump-game-ii/jump-game-ii.cpp
@@ -1,13 +1,40 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
+        return bottom_up_driver(nums);
+        // return top_down_driver(nums);
+    }
+    
+private:
+    int top_down_driver(vector<int> &nums) {
         if (nums.size() <= 1)
             return 0;
         vector<int> dp(nums.size() + 1, -1);
         return solve(0, nums, dp);
     }
     
-private:
+    int bottom_up_driver(vector<int> &nums) {
+        return solve2(nums);
+    }
+    
+    //bottom up
+    int solve2(vector<int> &nums) {
+        vector<int> dp(nums.size() + 1, 1e9);
+        dp[0] = 0;
+        for (int i = 0 ; i < nums.size() ; i++) {
+            for (int j = 1 ; j <= nums[i] ; j++) {
+                if (i + j < nums.size() - 1) {
+                    dp[i + j] = min(dp[i + j], dp[i] + 1);
+                }
+                else {
+                    dp[nums.size() - 1] = min(dp[nums.size() - 1], dp[i] + 1);
+                }
+            }
+        }
+        return dp[nums.size() - 1];
+    }
+    
+    //top down
     int solve(int ind, vector<int> &nums, vector<int> &dp) {
         if (ind >= nums.size() - 1)
             return 0;
