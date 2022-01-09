6018ae8754d58ecd63c78d8e315f491e5350c425
Ingy Badawi <ngbadawi@yahoo.com>
2022-01-09 02:05:39 +0200

    Time: 204 ms (41.87%), Space: 139.5 MB (22.84%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        int n = s.length();
+        vector<vector<bool>> dp (n, vector<bool> (n, false));
+        set_dp_table(s, dp);
+        vector<string> path;
+        vector<vector<string>> ans;
+        dfs(0, s, path, ans, dp);
+        return ans;
+    }
+    
+private:
+    void dfs(int start_ind, string &s, vector<string> path, vector<vector<string>> &ans,
+             vector<vector<bool>> &dp) {
+        if (start_ind == s.length()) {
+            ans.push_back(path);
+            return;
+        }
+        vector<bool> curr = dp[start_ind];
+        for (int i = start_ind ; i < curr.size() ; i++) {
+            if (dp[start_ind][i]) {
+                path.push_back(s.substr(start_ind, i - start_ind + 1));
+                dfs(i + 1, s, path, ans, dp);
+                path.pop_back();
+            }
+        }
+    }
+    
+    void set_dp_table(string &s, vector<vector<bool>> &dp) {
+        int n = s.length();
+        for (int i = 0 ; i < n ; i++)
+            dp[i][i] = true;
+        
+        for (int i = 0 ; i < n - 1 ; i++) {
+            if (s[i] == s[i + 1])
+                dp[i][i + 1] = true;
+        }
+        
+        for (int len = 3 ; len <= n ; len++) {
+            for (int i = 0 ; i < n - len + 1 ; i++) {
+                int st = i, ed = i + len - 1;
+                if (dp[st + 1][ed - 1] && s[st] == s[ed])
+                    dp[st][ed] = true;
+            }
+        }
+    }
+};
