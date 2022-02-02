762a0c6d2347f45b9286d4b5655977f3af0d502e
Ingy Badawi <ngbadawi@yahoo.com>
2022-02-02 02:45:36 +0200

    Time: 120 ms (73.23%), Space: 93.4 MB (53.53%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<int> mxs(prices.size());
         int mx_so_far = -1e9, best_profit = -1e9;
         for(int i = prices.size() - 1 ; i >= 0 ; i--) {
             mx_so_far = max(mx_so_far, prices[i]);
-            mxs[i] = mx_so_far;
-            best_profit = max(best_profit, mxs[i] - prices[i]);
+            best_profit = max(best_profit, mx_so_far - prices[i]);
         }
         return max(best_profit, 0);
     }
