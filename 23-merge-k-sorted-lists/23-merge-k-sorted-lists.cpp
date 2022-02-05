153ab174c013deef1530792191085af936e9e13e
Ingy Badawi <ngbadawi@yahoo.com>
2022-02-06 01:54:16 +0200

    Time: 1473 ms (5.01%), Space: 14.1 MB (11.41%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -11,50 +11,49 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
-        int k = lists.size();
-        int total = 0;
-        vector<ListNode *> heads;
-        for (int i = 0 ; i < k ; i++){
-            total += append_infinity(lists[i]);
-            heads.push_back(lists[i]);
-        }
-        
-        for (int i = 0 ; i < k ; i++) {
-            if (lists[i]->val != 1e5)
-                total--;
-            pq.push({lists[i]->val, i});
-            heads[i] = heads[i]->next;
-        }
-        
+        int total_count = append_infinity(lists);
         ListNode *ans = new ListNode(0);
         ListNode *temp = ans;
-        while (pq.size() && pq.top().first != 1e5) {
-            pair<int, int> curr = pq.top();
-            int value = curr.first, list_index = curr.second;
-            pq.pop();
-            if (value != 1e5) {
-                temp->next = new ListNode(value);
-                temp = temp->next;
-                pq.push({heads[list_index]->val, list_index});
-                heads[list_index] = heads[list_index]->next;
-                total--;
-            }
+        int done = 0;
+        while (done < total_count) {
+            int list_index = get_min_index(lists);
+            if (list_index == -1)
+                cout<<"oh no"<<endl;
+            
+            temp->next = new ListNode(lists[list_index]->val);
+            temp = temp->next;
+            lists[list_index] = lists[list_index]->next;
+            done++;
         }
         return ans->next;
     }
 
 private:
-    int append_infinity(ListNode *&head) {
-        ListNode *temp = head;
-        if (!head) {
-            head = new ListNode(1e5);
-            return 0;
+    int get_min_index(vector<ListNode*> &lists) {
+        int mn = 1e9, min_ind = -1;
+        for (int i = 0 ; i < lists.size() ; i++) {
+            if (lists[i] && lists[i]->val < mn) {
+                min_ind = i;
+                mn = lists[i]->val;
+            }
         }
-        int cnt = 1;
-        while (temp->next) 
-            temp = temp->next, cnt++;
-        temp->next = new ListNode(1e5);
-        return cnt;
+        return min_ind;
+    }
+    
+    int append_infinity(vector<ListNode*> &lists) {
+        int total_count = 0;
+        for (ListNode *list : lists) {
+            if (!list) {
+                list = new ListNode(1e9);
+                continue;
+            }
+            int list_cnt = 1;
+            ListNode *temp = list;
+            while(temp->next)
+                temp = temp->next, list_cnt++;
+            temp->next = new ListNode(1e9);
+            total_count += list_cnt;
+        }
+        return total_count;
     }
 };
