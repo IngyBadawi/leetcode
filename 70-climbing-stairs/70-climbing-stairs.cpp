249636b81485820e14a77dbbf658602d9fd3fb8e
Ingy Badawi <ngbadawi@yahoo.com>
2022-02-27 00:53:36 +0200

    Time: 3 ms (26.02%), Space: 6 MB (37.13%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1, dp[1] = 2;
+        for (int i = 2 ; i < n ; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+            cout<<"dp["<<i<<"] = "<<dp[i]<<endl;
+        }
+        return dp[n - 1];
+    }
+    
+    int climbStairs_topdown(int n) {
         vector<int> dp(n + 1, -1);
-        return solve(n, dp);
+        return solve_topdown(n, dp);
     }
     
 private:
-    int solve(int rem, vector<int> &dp) {
+    int solve_topdown(int rem, vector<int> &dp) {
         if (rem < 0)
             return 0;
         if (rem == 0)
@@ -15,8 +25,8 @@ private:
             return dp[rem];
         
         int ans = 0;
-        ans += solve(rem - 1, dp);
-        ans += solve(rem - 2, dp);
+        ans += solve_topdown(rem - 1, dp);
+        ans += solve_topdown(rem - 2, dp);
         
         return dp[rem] = ans;
     }
