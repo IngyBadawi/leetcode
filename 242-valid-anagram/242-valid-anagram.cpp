ba9c67bd2ab712af954a5062e83223babe4d8525
Ingy Badawi <ngbadawi@yahoo.com>
2022-03-07 02:58:47 +0200

    Time: 19 ms (29.32%), Space: 7.5 MB (8.33%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        
+        // return solution_one(s, t);
+        return solution_two(s, t);
+    }
+    
+private:
+    bool solution_two(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for (int i = 0 ; i < s.length() ; i++) {
+            if (s[i] != t[i])
+                return false;
+        }
+        return true;
+    }
+    
+    bool solution_one(string s, string t) {
         vector<int> f1 = get_freq(s);
         vector<int> f2 = get_freq(t);
         return compare(f1, f2);
     }
     
-private:
     vector<int> get_freq(string s) {
         vector<int> freq(26, 0);
         for (char c : s) 
