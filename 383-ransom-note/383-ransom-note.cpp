96b13a579d50b8d406ef3a25e5d9b1ff7c2be275
Ingy Badawi <ngbadawi@yahoo.com>
2022-03-07 04:37:02 +0200

    Time: 83 ms (5.05%), Space: 9.1 MB (5.52%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
+        // return sol_one(ransomNote, magazine);
+        return sol_two(ransomNote, magazine);
+    }
+    
+private:
+    bool sol_two(string ransomNote, string magazine) {
+        sort(ransomNote.begin(), ransomNote.end());
+        sort(magazine.begin(), magazine.end());
+        
+        int p1 = 0, p2 = 0;
+        while (p1 < ransomNote.length() && p2 < magazine.length()) {
+            if (ransomNote[p1] < magazine[p2])
+                return false;
+            if (ransomNote[p1] == magazine[p2]) {
+                p1++;
+                p2++;
+            }
+            else {
+                p2++;
+            }
+        }
+        return p1 == ransomNote.length();
+    }
+    
+    bool sol_one(string ransomNote, string magazine) {
         vector<int> ransom_freq = get_freq(ransomNote);
         vector<int> mag_freq = get_freq(magazine);
         
@@ -12,7 +37,6 @@ public:
         return true;
     }
     
-private:
     vector<int> get_freq(string s) {
         vector<int> freq(26, 0);
         for (char c : s)
