b1ed00c9a233efed46a4d0e814eb43702eb85a77
Ingy Badawi <ngbadawi@yahoo.com>
2022-04-02 01:03:07 +0200

    Time: 47 ms (13.00%), Space: 23.3 MB (41.69%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -3,9 +3,7 @@ public:
     void reverseString(vector<char>& s) {
         int l = 0, r = s.size() - 1;
         while(l <= r) {
-            char h = s[l];
-            s[l] = s[r];
-            s[r] = h;
+            swap(s[l], s[r]);
             l++, r--;
         }
     }
